d8e597a0d3499f1ee5f149ef2d55a06e84196eb5
KellerMarika <marikakeller92@gmail.com>
2023-04-17 11:24:52 +0200

    milestone 1 & 2 to fix data(catch errors)

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        private string titolo;
+        public string Titolo {
+            get => titolo;     
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)) titolo = value;
+                else Console.WriteLine($"'{value}' non è un dato valido");
+            }
+        }
+        private DateTime data;
+        public DateTime Data
+        {
+            get => data;
+            set
+            {
+                var data_corrente = DateTime.Now;
+                if (data_corrente < value) data = value;
+                else Console.WriteLine($"'{value.ToString("dd/MM/yyyy")}' data inserita già passata");
+            }
+        }
+
+        private int capienza;
+        public int Capienza { get => capienza;
+            private set
+            {
+                if (value > 0) capienza = value;
+                else Console.WriteLine($"il valore '{value}' inserito per la capienza dell'evento è minore di 0!");
+            }
+        }
+        public int Prenotazioni { get; private set;}
+
+
+        public int GetDisponibilita() => Capienza - Prenotazioni;
+
+        public Evento(string _titolo, DateTime _data, int _capienza)
+        {
+            Titolo = _titolo;
+            Data = _data;
+            Capienza = _capienza;
+        }
+
+        public void PrenotaPosti()
+        {
+            if(Capienza != Prenotazioni)
+            {
+                Console.Write($"Ci sono {GetDisponibilita()} posti disponibili alla prenotazione. Quanti desideri prenotarne?");
+                int posti = Convert.ToInt32(Console.ReadLine());
+
+                if (posti > 0)
+                {
+                    if (posti <= GetDisponibilita())
+                    {
+                        Prenotazioni += posti;
+                        Console.WriteLine($"'{posti}' prenotazioni effettuate correttamente. posti ancora disponibili: {GetDisponibilita()}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"I posti disponibili sono {GetDisponibilita()}. Si desidera prenotarli tutti?(si|no)");
+                        string confirmDelete = Console.ReadLine();
+                        if (confirmDelete == "si")
+                        {
+                            Prenotazioni = Capienza;
+                            Console.WriteLine("l'evento è tutto Esaurito!");
+                        }
+                        else Console.WriteLine("nessuna prenotazione fatta");
+                    }
+                }
+                else Console.WriteLine($"Attenzione: il numero di posti inserito '{posti}' è negativo. nessuna prenotazione portata a termine");
+            }
+            else Console.WriteLine("Spiacenti, l'evento è tutto Esaurito!");
+        }
+
+        public void DisdiciPosti()
+        {
+            if(Prenotazioni != 0)
+            {
+                Console.Write($"Ci sono {Prenotazioni} posti prenotati. Quanti desideri Disdirne?");
+                int posti = Convert.ToInt32(Console.ReadLine());
+
+                if (posti > 0)
+                {
+                    if (posti <= Prenotazioni)
+                    {
+                        Prenotazioni -= posti;
+                        Console.WriteLine($"'{posti}' prenotazioni disdette correttamente. ne restano {Prenotazioni}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"I posti prenotati sono {Prenotazioni}. Disdire tutte le prenotazioni?(si|no)");
+                        string confirmDelete = Console.ReadLine();
+                        if (confirmDelete == "si")
+                        {
+                            Prenotazioni = 0;
+                            Console.WriteLine("le prenotazioni sono azzerate");
+                        }
+                        else Console.WriteLine("nessuna prenotazione disdetta");
+                    }
+                }
+                else Console.WriteLine($"Attenzione: il numero di posti inserito '{posti}' non è valido. nessuna preotazione è stata disdetta!");
+            }
+            else { Console.WriteLine("spiacente, non ci sono prenotazioni da cancellare!"); }
+
+        }
+
+        public override string ToString()=> Titolo + " - " + Data.ToString("dd/MM/yyyy");
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace GestoreEventi
+﻿using System.Security.Cryptography;
+
+namespace GestoreEventi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Console.WriteLine("Crea un evento.");
+            //Console.Write("Inserisci il Titolo dell'evento:");
+            //string t1=Console.ReadLine();
+
+            ////bisognerebbe effettuare un controllo anche sui dati inseriti per creare la data
+            //int[] d1= new int[3];
+
+            //Console.WriteLine("Scegli la Data");
+            //Console.Write("giorno: ");
+            //d1[2]=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Mese: ");
+            //d1[1] = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Anno: ");
+            //d1[0] = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Scegli la Capienza del tuo evento:");
+            //int c1 = Convert.ToInt32(Console.ReadLine());
+
+            //Evento e1= new Evento(t1, new DateTime(d1[0], d1[1], d1[2]), c1);
+
+            Evento e1 = new Evento("evento1", new DateTime(2023, 1, 12), 10);
+
+            
+            Console.WriteLine(e1.Titolo);
+            Console.WriteLine(e1.ToString());
+            Console.WriteLine(e1.Data);
+            Console.WriteLine("capienza: "+e1.Capienza);
+            Console.WriteLine("disponibilità "+e1.GetDisponibilita());
+          
+
+            Console.WriteLine("desidere effettuare ina prenotazione o disdirne una? (p|d)");
+            string resp1 = Console.ReadLine();
+            if (resp1 == "p") e1.PrenotaPosti();
+            else if (resp1 == "d") e1.DisdiciPosti();
+
+            Console.WriteLine("capienza: " + e1.Capienza);
+            Console.WriteLine("prenotazioni "+e1.Prenotazioni);
+            Console.WriteLine("disponibilità " + e1.GetDisponibilita());
+
+            Console.WriteLine("desidere effettuare ina prenotazione o disdirne una? (p|d)");
+            string resp2 = Console.ReadLine();
+            if (resp2 == "p") e1.PrenotaPosti();
+            else if (resp2 == "d") e1.DisdiciPosti();
+
+            Console.WriteLine("capienza: " + e1.Capienza);
+            Console.WriteLine("prenotazioni " + e1.Prenotazioni);
+            Console.WriteLine("disponibilità " + e1.GetDisponibilita());
+
         }
     }
 }
