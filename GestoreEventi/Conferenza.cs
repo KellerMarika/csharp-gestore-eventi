862eff2a40e4e0f40068eb2d979bd3c53569e7a0
KellerMarika <marikakeller92@gmail.com>
2023-04-17 16:25:56 +0200

    bonus ok finito

---
+++ GestoreEventi/Conferenza.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Conferenza : Evento
+    {
+        private string relatore;
+
+        public string Relatore
+        {
+            get => relatore;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)) relatore = value;
+                else throw new Exception($"il nominativo del relatore inserito '{value}' non è valido");
+            }
+        }
+        private double prezzo;
+        public string Prezzo
+        {
+            get => prezzo.ToString("F2") + " euro";
+            set
+            {
+                try
+                {
+                    prezzo = Convert.ToDouble (value);
+                }
+                catch (Exception)
+                {
+                    throw new Exception($"'{value}' non è un dato valido"); ;
+                } 
+            }
+        }
+        
+
+        public Conferenza(string _titolo, DateTime _data, int _capienza, string _relatore, double _prezzo) : base(_titolo, _data, _capienza)
+        {
+            Relatore = _relatore;
+            Prezzo = _prezzo.ToString("F2");
+        }
+        public Conferenza(string _titolo, DateTime _data, int _capienza, string _relatore, int _prezzo) : base(_titolo, _data, _capienza)
+        {
+            Relatore = _relatore;
+            Prezzo = _prezzo.ToString("F2");
+        }
+
+        public string getData() =>Data.ToString("dd/M/yyyy");
+        public string getTime() => Data.ToString("HH:mm");
+
+        public override string ToString()
+        {
+            return base.ToString() +" - " + Relatore +" - "+ Prezzo;
+        }
+
+    }
+
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -7,43 +7,43 @@ namespace GestoreEventi
     {
         static void Main(string[] args)
         {
-            //MILESTONE 2 -------------------------------------------------------
-            //Console.WriteLine("Crea un evento.");
-            //Console.Write("Inserisci il Titolo dell'evento:");
-            //string t1=Console.ReadLine();
+            MILESTONE 2------------------------------------------------------ -
+            Console.WriteLine("Crea un evento.");
+            Console.Write("Inserisci il Titolo dell'evento:");
+            string t1 = Console.ReadLine();
 
-            ////bisognerebbe effettuare un controllo anche sui dati inseriti per creare la data
-            //int[] d1= new int[3];
+            //bisognerebbe effettuare un controllo anche sui dati inseriti per creare la data
+            int[] d1 = new int[3];
 
-            //Console.WriteLine("Scegli la Data");
-            //Console.Write("giorno: ");
-            //d1[2]=Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Mese: ");
-            //d1[1] = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Anno: ");
-            //d1[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Scegli la Data");
+            Console.Write("giorno: ");
+            d1[2] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Mese: ");
+            d1[1] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Anno: ");
+            d1[0] = Convert.ToInt32(Console.ReadLine());
 
-            //Console.Write("Scegli la Capienza del tuo evento:");
-            //int c1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Scegli la Capienza del tuo evento:");
+            int c1 = Convert.ToInt32(Console.ReadLine());
 
-            //Evento e1= new Evento(t1, new DateTime(d1[0], d1[1], d1[2]), c1);
-
-
-            ////Console.WriteLine(e1.Titolo);
-            ////Console.WriteLine(e1.ToString());
-            ////Console.WriteLine(e1.Data);
-            ////Console.WriteLine("capienza: "+e1.Capienza);
-            ////Console.WriteLine("disponibilità "+e1.GetDisponibilita());
+            Evento e1 = new Evento(t1, new DateTime(d1[0], d1[1], d1[2]), c1);
 
 
-            //Console.WriteLine("desidere effettuare ina prenotazione o disdirne una? (p|d)");
-            //string resp1 = Console.ReadLine();
-            //if (resp1 == "p") e1.PrenotaPosti();
-            //else if (resp1 == "d") e1.DisdiciPosti();
+            //Console.WriteLine(e1.Titolo);
+            //Console.WriteLine(e1.ToString());
+            //Console.WriteLine(e1.Data);
+            //Console.WriteLine("capienza: "+e1.Capienza);
+            //Console.WriteLine("disponibilità "+e1.GetDisponibilita());
 
-            //Console.WriteLine("capienza: " + e1.Capienza);
-            //Console.WriteLine("prenotazioni "+e1.Prenotazioni);
-            //Console.WriteLine("disponibilità " + e1.GetDisponibilita());
+
+            Console.WriteLine("desidere effettuare ina prenotazione o disdirne una? (p|d)");
+            string resp1 = Console.ReadLine();
+            if (resp1 == "p") e1.PrenotaPosti();
+            else if (resp1 == "d") e1.DisdiciPosti();
+
+            Console.WriteLine("capienza: " + e1.Capienza);
+            Console.WriteLine("prenotazioni " + e1.Prenotazioni);
+            Console.WriteLine("disponibilità " + e1.GetDisponibilita());
 
 
             //MILESTONE 4 --------------------------------------------------------------------
@@ -51,15 +51,15 @@ namespace GestoreEventi
 
             Console.WriteLine("MILESTONE 4 -----------------------");
             Console.WriteLine("Inserisci un titolo per il tuo Programma di Eventi");
-            string ProgramTitle=Console.ReadLine();
-            ProgrammaEventi userEventProgram= new ProgrammaEventi(ProgramTitle);
+            string ProgramTitle = Console.ReadLine();
+            ProgrammaEventi userEventProgram = new ProgrammaEventi(ProgramTitle);
             Console.WriteLine($"quanti eventi desideri aggiungere al programma '{userEventProgram.Titolo}'?");
             int ProgramEventsCount;
             int.TryParse(Console.ReadLine(), out ProgramEventsCount);
 
-            while (userEventProgram.GetEventsCount()< ProgramEventsCount)
+            while (userEventProgram.GetEventsCount() < ProgramEventsCount)
             {
-                Console.WriteLine($"Crea il tuo {userEventProgram.GetEventsCount()+1}° evento");
+                Console.WriteLine($"Crea il tuo {userEventProgram.GetEventsCount() + 1}° evento");
                 Console.Write("Inserisci il Titolo dell'evento:");
                 string t1 = Console.ReadLine();
 
@@ -90,10 +90,10 @@ namespace GestoreEventi
                     Console.WriteLine("errore: " + e.Message, e.InnerException);
                     Console.WriteLine("OPERAZIONE FALLITA");
                 }
-              //  finally { Console.WriteLine("EVENTO CREATO CORRETTAMENTOE!"); }
+                //  finally { Console.WriteLine("EVENTO CREATO CORRETTAMENTOE!"); }
 
-                }
-            Console.WriteLine("numero di eventi inseriti: "+userEventProgram.GetEventsCount());
+            }
+            Console.WriteLine("numero di eventi inseriti: " + userEventProgram.GetEventsCount());
             Console.WriteLine(userEventProgram.GetResume());
 
             Console.WriteLine("ricerca eventi per data:");
@@ -106,16 +106,23 @@ namespace GestoreEventi
             Console.Write("Anno: ");
             df[0] = Convert.ToInt32(Console.ReadLine());
 
-          var data = new DateTime(df[0], df[1], df[2]); 
+            var data = new DateTime(df[0], df[1], df[2]);
 
-          List<Evento> filtredList = userEventProgram.FiltredEventsByDate(data);
-       
+            List<Evento> filtredList = userEventProgram.FiltredEventsByDate(data);
+
             Console.WriteLine($"ci sono {filtredList.Count()}  Eventi presenti in {userEventProgram.Titolo} che avvengono in data: {data.ToString()}");
             Console.WriteLine(ProgrammaEventi.GetEvents(filtredList));
-            userEventProgram.ClearEvents();
-        
+            // userEventProgram.ClearEvents();
 
+            Console.WriteLine("BONUS -----------------------");
+            Conferenza co1 =new Conferenza("t1",new DateTime(2024,02,20),10,"Pinco Pallo", 22);
+            userEventProgram.AddEvent(co1);
 
+            Console.WriteLine($"ELENCO PROGRAMMA {userEventProgram.Titolo.ToUpper()} EVENTI & CONFERENZE");
+            foreach (var item in userEventProgram.Eventi)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
     }
 }
