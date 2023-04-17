387d3df4c6d0a778d4cdb7341e6bc19a1eac7b65
KellerMarika <marikakeller92@gmail.com>
2023-04-17 14:14:27 +0200

    milestone 3 ok

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -27,33 +27,60 @@ namespace GestoreEventi
 
             //Evento e1= new Evento(t1, new DateTime(d1[0], d1[1], d1[2]), c1);
 
-            Evento e1 = new Evento("evento1", new DateTime(2023, 1, 12), 10);
+            Evento e1 = new Evento("evento1", new DateTime(2023, 7, 12), 10);
 
-            
-            Console.WriteLine(e1.Titolo);
-            Console.WriteLine(e1.ToString());
-            Console.WriteLine(e1.Data);
-            Console.WriteLine("capienza: "+e1.Capienza);
-            Console.WriteLine("disponibilità "+e1.GetDisponibilita());
-          
+            //Console.WriteLine(e1.Titolo);
+            //Console.WriteLine(e1.ToString());
+            //Console.WriteLine(e1.Data);
+            //Console.WriteLine("capienza: "+e1.Capienza);
+            //Console.WriteLine("disponibilità "+e1.GetDisponibilita());
 
-            Console.WriteLine("desidere effettuare ina prenotazione o disdirne una? (p|d)");
-            string resp1 = Console.ReadLine();
-            if (resp1 == "p") e1.PrenotaPosti();
-            else if (resp1 == "d") e1.DisdiciPosti();
 
-            Console.WriteLine("capienza: " + e1.Capienza);
-            Console.WriteLine("prenotazioni "+e1.Prenotazioni);
-            Console.WriteLine("disponibilità " + e1.GetDisponibilita());
+            //Console.WriteLine("desidere effettuare ina prenotazione o disdirne una? (p|d)");
+            //string resp1 = Console.ReadLine();
+            //if (resp1 == "p") e1.PrenotaPosti();
+            //else if (resp1 == "d") e1.DisdiciPosti();
 
-            Console.WriteLine("desidere effettuare ina prenotazione o disdirne una? (p|d)");
-            string resp2 = Console.ReadLine();
-            if (resp2 == "p") e1.PrenotaPosti();
-            else if (resp2 == "d") e1.DisdiciPosti();
+            //Console.WriteLine("capienza: " + e1.Capienza);
+            //Console.WriteLine("prenotazioni "+e1.Prenotazioni);
+            //Console.WriteLine("disponibilità " + e1.GetDisponibilita());
 
-            Console.WriteLine("capienza: " + e1.Capienza);
-            Console.WriteLine("prenotazioni " + e1.Prenotazioni);
-            Console.WriteLine("disponibilità " + e1.GetDisponibilita());
+            //Console.WriteLine("desidere effettuare ina prenotazione o disdirne una? (p|d)");
+            //string resp2 = Console.ReadLine();
+            //if (resp2 == "p") e1.PrenotaPosti();
+            //else if (resp2 == "d") e1.DisdiciPosti();
+
+            //Console.WriteLine("capienza: " + e1.Capienza);
+            //Console.WriteLine("prenotazioni " + e1.Prenotazioni);
+            //Console.WriteLine("disponibilità " + e1.GetDisponibilita());
+
+
+                  
+            var data = new DateTime(2023, 7, 12);
+  
+            ProgrammaEventi myProgram= new ProgrammaEventi("Programma eventi");
+            myProgram.AddEvent(e1);
+            myProgram.AddEvent(new Evento("evento2", new DateTime(2023, 4, 30), 20));
+            myProgram.AddEvent (new Evento("evento3", new DateTime(2023, 7, 12), 30));
+
+
+            List<Evento> filtredList = myProgram.FiltredEventsByDate(new DateTime(2023, 7, 12));
+
+            foreach (var item in filtredList)
+            {
+                //Console.WriteLine(item.ToString());
+                //Console.WriteLine(item.GetDisponibilita());
+            }
+
+            string overview = ProgrammaEventi.GetEvents(filtredList);
+            Console.WriteLine(overview);
+
+            Console.WriteLine( myProgram.GetEventsCount());
+            myProgram.ClearEvents();
+            Console.WriteLine( myProgram.GetEventsCount());
+
+            Console.WriteLine( myProgram.GetResume());
+ 
 
         }
     }
---
+++ GestoreEventi/ProgrammaEventi.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class ProgrammaEventi
+    {
+        private string titolo;
+        public string Titolo
+        {
+            get => titolo;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)) titolo = value;
+                else Console.WriteLine($"'{value}' non è un dato valido");
+            }
+        }
+
+        public List <Evento> Eventi { get; private set; }
+
+
+        public ProgrammaEventi(string _titolo)
+        {
+            Titolo = _titolo;
+            Eventi = new List <Evento>();
+        }
+
+
+        public void AddEvent(Evento evento)=> Eventi.Add(evento);
+
+        public List<Evento> FiltredEventsByDate(DateTime data)
+        {
+            List<Evento> filtredList = new List <Evento>();  
+            foreach (var evento in Eventi)
+            {
+                if (evento.Data == data)
+                 filtredList.Add(evento);               
+            }
+            return filtredList;
+        }
+
+        public static string GetEvents(List<Evento> events)
+        {
+            string[] overvieusArray =new string[events.Count()];
+  
+            for (int i = 0; i<events.Count; i++)
+            {
+              overvieusArray[i] ="Evento n#"+ i + ": " + events[i].ToString() + " -  prenotati " + events[i].Prenotazioni + "/" + events[i].Capienza;
+            }
+                return String.Join(", ",overvieusArray); 
+        }
+
+        public int GetEventsCount() => Eventi.Count();
+        
+        public void ClearEvents()=>  Eventi.Clear();
+
+        public string GetResume()
+        {
+            string[] overvieusArray = new string[Eventi.Count()];
+
+   
+            if (Eventi.Count() > 0)
+            {
+                for (int i = 0; i < Eventi.Count(); i++)
+                {
+                    overvieusArray[i] = "Evento n#" + i + ": " + Eventi[i].ToString() + " -  prenotati " + Eventi[i].Prenotazioni + "/" + Eventi[i].Capienza;
+                }
+                return titolo.ToUpper() + ": " + String.Join(", ", overvieusArray);
+            }
+            else return titolo.ToUpper() + ": non ci sono eventi in programma!";
+        }
+    }
+}
